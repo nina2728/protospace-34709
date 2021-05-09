fea8c66d03761b4f3029fda81248b2ac4be9699f
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-05-09 18:13:05 +0900

    アクセス制限をした。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, only: [:edit] 
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -45,5 +48,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
